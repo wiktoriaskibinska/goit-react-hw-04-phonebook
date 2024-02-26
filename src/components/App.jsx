3ec2c3b2c983c49437219a093dacba2b4748ee59
Wiktoria Skibińska <wikas2000@icloud.com>
2024-02-26 15:07:56 +0100

    use Effect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,6 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 
 export const App = () => {
-  const phoneContacts = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
   const [contacts, setContacts] = useState(() => {
     return JSON.parse(localStorage.getItem('contacts'));
   });
